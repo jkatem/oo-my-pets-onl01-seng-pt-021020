dad0bd81764e59a69e7c76af493d62d59d1ed5c2
Jen Miao <jenmiao@gmail.com>
2020-03-11 05:36:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = 'human'
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    # @@all.each_with_index do { |owner| remove_method(owner)}
+    self.all.clear
+  end
+
+
+  #binding.pry
 end
